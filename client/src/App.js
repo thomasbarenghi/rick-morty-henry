1f36aeac7c10f4dad0109e867409ecd479849965
unknown <thomasbarenghi@gmail.com>
2023-03-10 11:10:18 -0300

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -12,26 +12,26 @@ import CharacterDetail from "./paginas/personajeDetail/personajeDetail";
 import FooterDefault from "./componentes/masters/footer/FooterDefault";
 import Login from "./paginas/auth/login";
 import ScrollToTop from "./componentes/scrollToTop/scrollToTop";
-
+import { useLocation } from "react-router-dom";
 
 
 function App() {
 
   const navigate = useNavigate();
-  const token = Cookies.get('token');
-  const userId = Cookies.get('userId');
+  const location = useLocation();
 
+  const { token, userId } = Cookies.get();
   const headers = { "Authorization": `Bearer ${token}`, };
 
   useEffect(() => {
 
     if ((!token || !userId) || (token === "" || userId === "")) { navigate("/auth/login"); return; }
-
+    //if (!token || !userId) { return Promise.reject(new Error('No hay token o userId en getFavorites')); }
     axios.get(`${SERVER_URL}/auth/me`, { headers })
       .then((response) => { console.log(response.data); })
       .catch((error) => { console.log("Error:", error); });
 
-  }, [token, userId]);
+  }, [ ]);
 
 
   return (
