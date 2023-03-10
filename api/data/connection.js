a8a7173d9fa7fcd626a0ff4de1a2292025f6de69
unknown <thomasbarenghi@gmail.com>
2023-03-09 23:37:34 -0300

    back

--- api/data/connection.js
+++ api/data/connection.js
@@ -2,56 +2,60 @@ const { Sequelize } = require('sequelize');
 const mode = require('../config/config');
 const path = require('path');
 
+if (mode === "postgreSQL") {
 
-if(mode === "postgre"){
+  const dbConfig = {
+    database: 'rickandmorty',
+    username: 'postgres',
+    password: 'admin',
+    host: 'localhost',
+    dialect: 'postgres'
+  };
 
-const sequelize = new Sequelize('rickandmorty', 'postgres', 'admin', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Conexión exitosa');
-
-    const tableNames = await sequelize.getQueryInterface().showAllTables();
-    console.log('Tablas disponibles:', tableNames);
-  } catch (error) {
-    console.error('Imposible conectar:', error);
-  }
-})();
-
-module.exports = sequelize;
-
-} else if(mode === "sqlite"){
-
-
-
-const dbPath = path.join(__dirname, 'sqlite.db');
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: dbPath
-});
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-    sequelize.query("SELECT name FROM sqlite_master WHERE type='table'", { type: sequelize.QueryTypes.SELECT })
-      .then((tables) => {
-        console.log('Tables:');
-        tables.forEach((table) => {
-          console.log(table.name);
-        });
-      });
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+  const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect
   });
 
-module.exports = sequelize;
+  (async () => {
+
+    try {
+
+      await sequelize.authenticate();
+      console.log('Conexión exitosa');
+
+      const tableNames = await sequelize.getQueryInterface().showAllTables();
+      console.log('Tablas disponibles:', tableNames);
+
+    } catch (error) { console.error('Imposible conectar:', error); }
+
+  })();
+
+  module.exports = sequelize;
 
 }
 
+else if (mode === "sqlite") {
 
+  const dbPath = path.join(__dirname, 'sqlite.db');
+
+  const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: dbPath
+  });
+
+  sequelize.authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+      sequelize.query("SELECT name FROM sqlite_master WHERE type='table'", { type: sequelize.QueryTypes.SELECT })
+        .then((tables) => {
+          console.log('Tables:'); tables.forEach((table) => { console.log(table.name); });
+        });
+    })
+    .catch(err => {
+      console.error('Unable to connect to the database:', err);
+    });
+
+  module.exports = sequelize;
+
+}
