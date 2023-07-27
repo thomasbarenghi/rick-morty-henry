6d397db0c2eb70712edafc811c24c3d53e67e8f4
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-07-27 00:26:22 -0300

    By Pass

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,36 @@
-import "./styles/global.scss";
-import "./styles/varios.scss";
-import "./styles/bootstrap.min.css";
-import { useEffect } from "react";
+import "./styles/global.scss"
+import "./styles/varios.scss"
+import "./styles/bootstrap.min.css"
+import { useEffect } from 'react';
 import { Routes, Route, useNavigate } from "react-router-dom";
-import Cookies from "js-cookie";
+import Cookies from "js-cookie"
 import axios from "axios";
 import { SERVER_URL } from "./api/config";
-import Home from "./paginas/home/home";
-import About from "./paginas/about/about";
+import Home from "./paginas/home/home"
+import About from "./paginas/about/about"
 import CharacterDetail from "./paginas/personajeDetail/personajeDetail";
 import FooterDefault from "./componentes/masters/footer/FooterDefault";
 import Login from "./paginas/auth/login";
 import ScrollToTop from "./componentes/scrollToTop/scrollToTop";
 
+
 function App() {
+
   const navigate = useNavigate();
 
   const { token, userId } = Cookies.get();
-  const headers = { Authorization: `Bearer ${token}` };
+  const headers = { "Authorization": `Bearer ${token}`, };
 
   useEffect(() => {
-    console.log("hola");
-    if (!token || !userId || token === "" || userId === "") {
-      navigate("/");
-      return;
-    }
-    if (!token || !userId) {
-      return Promise.reject(new Error("No hay token o userId en getFavorites"));
-    }
-    axios
-      .get(`${SERVER_URL}/auth/me`, { headers })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-  }, [navigate]);
+console.log("hola")
+    if ((!token || !userId) || (token === "" || userId === "")) { navigate("/auth/login"); return; }
+    //if (!token || !userId) { return Promise.reject(new Error('No hay token o userId en getFavorites')); }
+    axios.get(`${SERVER_URL}/auth/me`, { headers })
+      .then((response) => { console.log(response.data); })
+      .catch((error) => { console.log("Error:", error); });
+
+  }, [navigate, ]);
+
 
   return (
     <>
