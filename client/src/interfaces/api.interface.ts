0272efb3af62304ff29cf27f735504d532c5fee0
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2024-06-04 18:43:17 -0300

    simplificacion general del proyecto

--- client/src/interfaces/api.interface.ts
+++ client/src/interfaces/api.interface.ts
@@ -1,7 +1,4 @@
 /* eslint-disable @typescript-eslint/indent */
-import { type SupabaseClient, type PostgrestError } from '@supabase/supabase-js'
-import { type OrderInterface, type CartItem, type OrderStatusApiEnum } from './order.interface'
-import { type ProductFormData, type OrderFormProps, type ProfileFormData } from './form.interface'
 
 export interface Response<T> {
   data: T | null
@@ -14,7 +11,6 @@ export type GeneralError =
       code: number
     }
   | null
-  | PostgrestError
   | undefined
 
 export interface GetRequestParams {
@@ -35,81 +31,9 @@ export interface MutationRequestParams {
 
 export type HttpMethod = 'post' | 'put' | 'delete' | 'patch'
 
-export interface CreateOrderRequest {
-  products: CartItem[]
-  details: OrderFormProps
-}
-
-export interface GetPreferenceRequest {
-  products: CartItem[]
-  orderData: OrderInterface
-}
-
 export type OrderType = 'active' | 'finished' | 'all'
 
 export interface GetOrdersRequest {
   type: OrderType
   mode: 'shop' | 'user'
-}
-
-export interface OrderQueryParams {
-  supabase: SupabaseClient
-  userId?: string
-  type: OrderType
-}
-
-export interface ImageUploadRequest {
-  image: File | undefined
-}
-
-export interface ChangeOrderStatusRequest {
-  status: OrderStatusApiEnum
-  orderId: string
-}
-
-export interface GetProductRequest {
-  id: string
-}
-
-export interface GetProductsRequest {
-  ids?: string[] | null
-}
-
-export interface CreateProductRequest {
-  product: ProductFormData
-}
-
-export interface PatchProductRequest {
-  product: ProductFormData
-  id: string
-}
-
-export interface DeleteProductRequest {
-  id: string
-}
-
-export interface CreateMessageRequest {
-  orderId: string
-  message: string
-}
-
-export interface GetOrderMessagesRequest {
-  orderId: string
-}
-
-export interface ChangeShopStatusRequest {
-  isOpen: boolean
-}
-
-export interface CreateProfileRequest {
-  formData: ProfileFormData
-}
-
-export interface PatchUserProfile {
-  formData: ProfileFormData
-  id: string
-}
-
-export interface GetProfileRequest {
-  id?: string | null
 }
